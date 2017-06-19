de2e2b9b08833df7d7d97316c1ca1a3c836c7105
Amy Volkert <Amy Volkert>
2017-06-19 12:20:30 -0600

    Added CSS file, psuedocode, and table

---
+++ logic.js
@@ -0,0 +1,9 @@
+// Incorporate Moment.js 
+// Assumptions
+	// Trains: Thomas, Edward, Gordon, Emily, Henry, Toby, Percy 
+	// Destinations: Water Tower, Blue Mountain Mine, Roundhouse, Snow Tunnel, Rumblin' Bridge, Popcorn Factory, Sodor Paint Factory
+	// Frequecy: Every 8 minutes
+	// The first trains arrive at 4:05 a.m. 
+
+
+
