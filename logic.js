b9a46ff725666dc59463da31d0c1ab5bf8be46d0
Amy Volkert <Amy Volkert>
2017-06-19 13:59:56 -0600

    Initialized Firebase

--- logic.js
+++ logic.js
@@ -1,9 +1,20 @@
+// Initialize Firebase
+// Update Firebase db
 // Incorporate Moment.js 
-// Assumptions
+// Possible drop-down items
 	// Trains: Thomas, Edward, Gordon, Emily, Henry, Toby, Percy 
 	// Destinations: Water Tower, Blue Mountain Mine, Roundhouse, Snow Tunnel, Rumblin' Bridge, Popcorn Factory, Sodor Paint Factory
-	// Frequecy: Every 8 minutes
-	// The first trains arrive at 4:05 a.m. 
+	// Frequecy in minutes
+// Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDAcmlLVxHZGrhVvi1O1LymaWTELvZEx9o",
+    authDomain: "train-scheduler-fb9ec.firebaseapp.com",
+    databaseURL: "https://train-scheduler-fb9ec.firebaseio.com",
+    projectId: "train-scheduler-fb9ec",
+    storageBucket: "train-scheduler-fb9ec.appspot.com",
+    messagingSenderId: "639659276040"
+  };
+  firebase.initializeApp(config);
 
 
 
