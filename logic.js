f10eb7dcb31932753f68631305732de625446df7
Amy Volkert <amyvolkert@comcast.net>
2018-01-10 06:54:35 -0700

    first commit

--- logic.js
+++ logic.js
@@ -1,17 +1,17 @@
 // Update Firebase db
-// Incorporate Moment.js 
+// Incorporate Moment.js
 // Possible drop-down items
-	// Trains: Thomas, Edward, Gordon, Emily, Henry, Toby, Percy 
+	// Trains: Thomas, Edward, Gordon, Emily, Henry, Toby, Percy
 	// Destinations: Water Tower, Blue Mountain Mine, Roundhouse, Snow Tunnel, Rumblin' Bridge, Popcorn Factory, Sodor Paint Factory
 	// Frequecy in minutes
 // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyDAcmlLVxHZGrhVvi1O1LymaWTELvZEx9o",
-    authDomain: "train-scheduler-fb9ec.firebaseapp.com",
-    databaseURL: "https://train-scheduler-fb9ec.firebaseio.com",
-    projectId: "train-scheduler-fb9ec",
-    storageBucket: "train-scheduler-fb9ec.appspot.com",
-    messagingSenderId: "639659276040"
+var config = {
+    apiKey: "AIzaSyCqWe302zhuIwBHvHldf8uJoMPuGaP8Szs",
+    authDomain: "train-scheduler-d0466.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d0466.firebaseio.com",
+    projectId: "train-scheduler-d0466",
+    storageBucket: "",
+    messagingSenderId: "757381073056"
   };
   firebase.initializeApp(config);
   var database = firebase.database();
@@ -31,7 +31,7 @@
       frequency: howOften
     };
   // Uploading data to Firebase db
-  // database.ref references the root 
+  // database.ref references the root
   database.ref().push(newTrain);
   // Logging data to the console
   console.log(newTrain.name);
@@ -58,9 +58,6 @@ database.ref().on("child-added", function(childSnapshot, prevChildKey) {
   console.log(firstTime);
   console.log(howOften);
   // Add each train's data into the table
-  $("#train-table > tbody").append"<tr><td>" + trainName + 
+  $("#train-table > tbody").append"<tr><td>" + trainName +
   "</td><td>" + trainDestination "</td></tr>";
 });
-
-
-
